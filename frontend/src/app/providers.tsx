7500e570dcfb9075134f2e9fb534a9e161b140dd
keinsize <118171210+keinsize@users.noreply.github.com>
2024-11-15 01:23:53 +0300

    Update providers.tsx

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -2,14 +2,12 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { PropsWithChildren } from 'react'
-import { toastTest } from './toast-test'
 
 const queryClient = new QueryClient({
 	defaultOptions: { queries: { refetchOnWindowFocus: false, retry: 0 } }
 })
 
 export function Providers({ children }: PropsWithChildren) {
-	toastTest()
 
 	return (
 		<QueryClientProvider client={queryClient}>

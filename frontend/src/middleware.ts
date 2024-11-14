68d6c1389806aa64ce7432e69a6d8b57f404c456
keinsize <118171210+keinsize@users.noreply.github.com>
2024-11-15 01:47:10 +0300

    Update middleware.ts

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -7,7 +7,7 @@ export async function middleware(request: NextRequest, response: NextResponse) {
 	const refreshToken = cookies.get(EnumTokens.REFRESH_TOKEN)?.value
 
 	if (!refreshToken) {
-		return NextResponse.redirect(new URL('/auth', request.url))
+		return NextResponse.redirect(new URL('/', request.url))
 	}
 
 	return NextResponse.next()
